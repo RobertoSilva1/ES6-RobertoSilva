a9e0e3b42fe846c82b71914eb67bc1304e53eb17
RobertoSilva1 <69914704+RobertoSilva1@users.noreply.github.com>
2020-09-10 17:58:39 -0300

    subida tpl es6

---
+++ app.js
@@ -0,0 +1,31 @@
+
+const saludar = nombre =>'Hola ' + nombre;
+
+
+// Mostrar el resultado por consola
+console.log(saludar('Alejandro'));
+
+
+
+
+const multiplicar = (a, b) => a + b;
+
+// Mostrar el resultado por consola
+console.log(multiplicar(3, 15))
+
+
+
+
+const crearAlumno = (nombre, apellido, edad, materias) => ({nombre, apellido, edad, materias})
+
+// Mostrar el resultado por consola.
+console.log(crearAlumno('Leo', 'Messi', '32', ['TLPA','Base de Datos','Inglés']))
+
+
+
+
+
+const  devolverFecha = (...arguments) =>`Hoy es ${arguments[0]}-${arguments[1]}-${arguments[2]}`
+
+// Mostrar el resultado por consola.
+console.log(devolverFecha('10','09','2020'));
